181fe41f321b30341350dcbbe31249f6cd7efbdf
Crisiroid <csiroid@gmail.com>
2022-02-20 20:36:41 +0330

    updating the main class with the new class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,18 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
-        //calling binary class
         BinaryCal bc = new BinaryCal();
-        bc.calculate();
+        BinaryToDecimal btd = new BinaryToDecimal();
+        int input = Integer.parseInt(JOptionPane.showInputDialog("What do you want to do ? \n 1: convert decimal to binary \n 2: convert binary to decimal"));
+        switch (input){
+            case 1:
+                bc.calculate();
+                break;
+            case 2:
+                btd.calculate();
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Wrong number!");
+        }
     }
 }
